49ee049afb8a3ae9c54c65d4a7830545ace37f54
nameisjayant <jayntkumar99@gmail.com>
2023-09-28 21:47:29 +0530

    side nav bar added with navigation rail

--- app/src/main/java/com/nameisjayant/androidtv/layout/side_drawer.kt
+++ app/src/main/java/com/nameisjayant/androidtv/layout/side_drawer.kt
@@ -1,22 +1,31 @@
 package com.nameisjayant.androidtv.layout
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.DrawerValue
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.NavigationRail
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import com.nameisjayant.androidtv.R
 
@@ -28,11 +37,11 @@ val itemList = listOf(
     MenuItem(
         1, "Home", R.drawable.baseline_home_24
     ), MenuItem(
-        1, "Search", R.drawable.baseline_search_24
+        2, "Search", R.drawable.baseline_search_24
     ), MenuItem(
-        1, "Videos", R.drawable.baseline_video_call_24
+        3, "Videos", R.drawable.baseline_video_call_24
     ), MenuItem(
-        1, "Music", R.drawable.baseline_music_note_24
+        4, "Music", R.drawable.baseline_music_note_24
     )
 )
 
@@ -42,21 +51,16 @@ fun SideDrawerScreen(
     modifier: Modifier = Modifier
 ) {
     var selectedItems by remember { mutableStateOf(itemList[0].id) }
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
-
-    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
-        Column {
-            itemList.forEach {
-                NavigationRow(
-                    menuItem = it,
-                    isSelected = selectedItems == it.id,
-                    onMenuSelected = { item ->
-                        selectedItems = item.id
-                    })
-            }
+    NavigationRail(
+        modifier = modifier, containerColor = Color.Black.copy(alpha = 0.3f)
+    ) {
+        itemList.forEach {
+            NavigationRow(menuItem = it,
+                isSelected = selectedItems == it.id,
+                onMenuSelected = { item ->
+                    selectedItems = item.id
+                })
         }
-    }, modifier = modifier) {
-
     }
 
 }
@@ -69,19 +73,37 @@ fun NavigationRow(
     onMenuSelected: (MenuItem) -> Unit
 ) {
 
-    IconButton(
-        onClick = {
-            onMenuSelected(menuItem)
-        },
+    val tint by animateColorAsState(
+        targetValue = if (isSelected) Color.White else Color.Unspecified,
+        label = stringResource(R.string.animatedcolor)
+    )
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-            .padding(10.dp)
-            .size(24.dp),
+            .padding(horizontal = 10.dp, vertical = 12.dp)
+            .toggleable(
+                value = false,
+                onValueChange = { onMenuSelected(menuItem) },
+                role = Role.Button,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ),
     ) {
         Icon(
             painter = painterResource(id = menuItem.icon),
             contentDescription = null,
-            tint = if (isSelected) Color.White else Color.Unspecified
+            tint = tint
         )
+        Spacer(modifier = Modifier.height(5.dp))
+        AnimatedVisibility(visible = isSelected) {
+            Box(
+                modifier = Modifier
+                    .background(Color.White, RoundedCornerShape(16.dp))
+                    .width(15.dp)
+                    .height(4.dp)
+            )
+        }
     }
 
 }
